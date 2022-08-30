8d94c4808dd12fec09cceafeaa72873c7cb6500e
Olivier Robardet <olivier.robardet@gmail.com>
2022-08-30 17:49:57 +0200

    Update

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,5 @@
 Import-Module PSReadline
+Import-Module CompletionPredictor
 
 $Env:PATH += ";$Env:GOPATH\bin"
 
@@ -7,16 +8,22 @@ Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadLineOption -EditMode Emacs
 Set-PSReadlineOption -BellStyle None
 Set-PSReadLineOption -Colors @{ "Command"=[ConsoleColor]::DarkYellow }
+Set-PSReadlineOption -PredictionSource HistoryAndPlugin
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 
+## Posh-git and oh-my-posh are replaced with powerline-go blow
 # Install-Module posh-git -Scope CurrentUser
-Import-Module posh-git
+# Import-Module posh-git
 # Install-Module oh-my-posh -Scope CurrentUser
-Import-Module oh-my-posh
-Set-PoshPrompt -Theme Fish
+# Import-Module oh-my-posh
+# Set-PoshPrompt -Theme Fish
 
 # Load powerline-go prompt
 function global:prompt {
-    $pwd = $ExecutionContext.SessionState.Path.CurrentLocation
+    # $pwd = $ExecutionContext.SessionState.Path.CurrentLocation
     $startInfo = New-Object System.Diagnostics.ProcessStartInfo
     $startInfo.FileName = "powerline-go"
     $startInfo.Arguments = "-shell bare -modules termtitle,exit,docker,venv,vgo,newline,host,perms,cwd,git,newline,jobs,time,root -cwd-max-depth 5 -max-width 100 -shorten-gke-names"
@@ -25,7 +32,7 @@ function global:prompt {
     $startInfo.StandardOutputEncoding = [System.Text.Encoding]::UTF8
     $startInfo.RedirectStandardOutput = $true
     $startInfo.UseShellExecute = $false
-    $startInfo.WorkingDirectory = $pwd
+    $startInfo.WorkingDirectory = $ExecutionContext.SessionState.Path.CurrentLocation
     $process = New-Object System.Diagnostics.Process
     $process.StartInfo = $startInfo
     $process.Start() | Out-Null
@@ -35,11 +42,12 @@ function global:prompt {
 }
 
 If (-Not (Test-Path Variable:PSise)) {  # Only run this in the console and not in the ISE
-	Import-Module -Name Terminal-Icons
+	# Import-Module -Name Terminal-Icons # disabled as it is slow 
     # Import-Module Get-ChildItemColor
     Set-Alias l Get-ChildItem -option AllScope
     Set-Alias ls Get-ChildItemColorFormatWide -option AllScope
 	Set-Alias ll l
+	Set-Alias spp Split-Path
 } Else {
 	Set-Alias ll ls
 	Set-Alias l ls
